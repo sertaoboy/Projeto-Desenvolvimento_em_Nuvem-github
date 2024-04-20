1f0bea0f883b76828d8f30926b3758109e4e4cf9
Raul Holanda <123959950+sertaoboy@users.noreply.github.com>
2024-04-20 16:22:11 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,21 @@
+// ARQUIVO index.js USADO NA AULA DE NODEJS COM QUERY PARAMS
+
+
+const express = require("express"); // importacao do modulo para o arquivo
+const app = express(); // criacao de uma instancia do express
+
+//rota com query params
+app.get("/consulta", function(req,res){
+    var cpf = req.query["cpf"];
+    if(cpf){
+        res.send("Consulta realizada com sucesso para o CPF: " + cpf);
+    }else{
+        res.send("CPF NAO FORNECIDO");
+    }
+})
+
+app.listen(4000, function(erro){
+    if(erro){
+        console.log("Ocorreu um erro");
+    }else{
+        console.log("Servidor iniciado com sucesso");
